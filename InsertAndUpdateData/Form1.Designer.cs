88f2f3f994c20ba932b464e9fa119aec6f474649
Shaun Rametsi <shaunus111@gmail.com>
2018-05-28 09:31:49 +0200

    initial commit

---
+++ InsertAndUpdateData/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace InsertAndUpdateData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cmbGenre = new System.Windows.Forms.ComboBox();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.gamerDetailsBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataDataSet3 = new InsertAndUpdateData.dataDataSet3();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.gamerDetailsBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataDataSet2 = new InsertAndUpdateData.dataDataSet2();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataDataSet = new InsertAndUpdateData.dataDataSet();
+            this.dataDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataDataSet1 = new InsertAndUpdateData.dataDataSet1();
+            this.gamerDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gamer_DetailsTableAdapter = new InsertAndUpdateData.dataDataSet1TableAdapters.Gamer_DetailsTableAdapter();
+            this.gamer_DetailsTableAdapter1 = new InsertAndUpdateData.dataDataSet2TableAdapters.Gamer_DetailsTableAdapter();
+            this.gamer_DetailsTableAdapter2 = new InsertAndUpdateData.dataDataSet3TableAdapters.Gamer_DetailsTableAdapter();
+            this.fillByToolStrip = new System.Windows.Forms.ToolStrip();
+            this.fillByToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gamerDetailsBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamerDetailsBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamerDetailsBindingSource)).BeginInit();
+            this.fillByToolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.cmbGenre);
+            this.panel1.Controls.Add(this.cmbGender);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(9, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(264, 289);
+            this.panel1.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Game Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Game Genre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name Surname";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // cmbGenre
+            // 
+            this.cmbGenre.FormattingEnabled = true;
+            this.cmbGenre.Location = new System.Drawing.Point(12, 148);
+            this.cmbGenre.Name = "cmbGenre";
+            this.cmbGenre.Size = new System.Drawing.Size(234, 21);
+            this.cmbGenre.TabIndex = 2;
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Location = new System.Drawing.Point(11, 198);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(235, 21);
+            this.cmbGender.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Impact", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "INSERT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.comboBox3);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(279, 31);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(271, 298);
+            this.panel2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DataSource = this.gamerDetailsBindingSource2;
+            this.comboBox3.DisplayMember = "First_Name";
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(9, 52);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(234, 21);
+            this.comboBox3.TabIndex = 17;
+            this.comboBox3.ValueMember = "First_Name";
+            // 
+            // gamerDetailsBindingSource2
+            // 
+            this.gamerDetailsBindingSource2.DataMember = "Gamer_Details";
+            this.gamerDetailsBindingSource2.DataSource = this.dataDataSet3;
+            // 
+            // dataDataSet3
+            // 
+            this.dataDataSet3.DataSetName = "dataDataSet3";
+            this.dataDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Game Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(9, 109);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(235, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // gamerDetailsBindingSource1
+            // 
+            this.gamerDetailsBindingSource1.DataMember = "Gamer_Details";
+            this.gamerDetailsBindingSource1.DataSource = this.dataDataSet2;
+            // 
+            // dataDataSet2
+            // 
+            this.dataDataSet2.DataSetName = "dataDataSet2";
+            this.dataDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Impact", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(10, 248);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Chiller", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Insert New Gamer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Chiller", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(286, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Update Gamer Info";
+            // 
+            // dataDataSet
+            // 
+            this.dataDataSet.DataSetName = "dataDataSet";
+            this.dataDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataDataSetBindingSource
+            // 
+            this.dataDataSetBindingSource.DataSource = this.dataDataSet;
+            this.dataDataSetBindingSource.Position = 0;
+            // 
+            // dataDataSet1
+            // 
+            this.dataDataSet1.DataSetName = "dataDataSet1";
+            this.dataDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // gamerDetailsBindingSource
+            // 
+            this.gamerDetailsBindingSource.DataMember = "Gamer_Details";
+            this.gamerDetailsBindingSource.DataSource = this.dataDataSet1;
+            // 
+            // gamer_DetailsTableAdapter
+            // 
+            this.gamer_DetailsTableAdapter.ClearBeforeFill = true;
+            // 
+            // gamer_DetailsTableAdapter1
+            // 
+            this.gamer_DetailsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // gamer_DetailsTableAdapter2
+            // 
+            this.gamer_DetailsTableAdapter2.ClearBeforeFill = true;
+            // 
+            // fillByToolStrip
+            // 
+            this.fillByToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fillByToolStripButton});
+            this.fillByToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.fillByToolStrip.Name = "fillByToolStrip";
+            this.fillByToolStrip.Size = new System.Drawing.Size(552, 25);
+            this.fillByToolStrip.TabIndex = 4;
+            this.fillByToolStrip.Text = "fillByToolStrip";
+            // 
+            // fillByToolStripButton
+            // 
+            this.fillByToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fillByToolStripButton.Name = "fillByToolStripButton";
+            this.fillByToolStripButton.Size = new System.Drawing.Size(39, 22);
+            this.fillByToolStripButton.Text = "FillBy";
+            this.fillByToolStripButton.Click += new System.EventHandler(this.fillByToolStripButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 329);
+            this.Controls.Add(this.fillByToolStrip);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gamerDetailsBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamerDetailsBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamerDetailsBindingSource)).EndInit();
+            this.fillByToolStrip.ResumeLayout(false);
+            this.fillByToolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cmbGenre;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.BindingSource dataDataSetBindingSource;
+        private dataDataSet dataDataSet;
+        private dataDataSet1 dataDataSet1;
+        private System.Windows.Forms.BindingSource gamerDetailsBindingSource;
+        private dataDataSet1TableAdapters.Gamer_DetailsTableAdapter gamer_DetailsTableAdapter;
+        private dataDataSet2 dataDataSet2;
+        private System.Windows.Forms.BindingSource gamerDetailsBindingSource1;
+        private dataDataSet2TableAdapters.Gamer_DetailsTableAdapter gamer_DetailsTableAdapter1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private dataDataSet3 dataDataSet3;
+        private System.Windows.Forms.BindingSource gamerDetailsBindingSource2;
+        private dataDataSet3TableAdapters.Gamer_DetailsTableAdapter gamer_DetailsTableAdapter2;
+        private System.Windows.Forms.ToolStrip fillByToolStrip;
+        private System.Windows.Forms.ToolStripButton fillByToolStripButton;
+    }
+}
+
---
+++ InsertAndUpdateData/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace InsertAndUpdateData
+{
+    public partial class Form1 : Form
+    {
+        static OleDbConnection connection = new OleDbConnection();
+        static OleDbCommand    command = new OleDbCommand();
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            connection.ConnectionString = @"Provider = Microsoft.ACE.OLEDB.12.0;" +
+                                          @"Data Source= ../data.accdb";
+            command.Connection = connection ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataDataSet3.Gamer_Details' table. You can move, or remove it, as needed.
+            this.gamer_DetailsTableAdapter2.Fill(this.dataDataSet3.Gamer_Details);
+            // TODO: This line of code loads data into the 'dataDataSet2.Gamer_Details' table. You can move, or remove it, as needed.
+            this.gamer_DetailsTableAdapter1.Fill(this.dataDataSet2.Gamer_Details);
+            // TODO: This line of code loads data into the 'dataDataSet1.Gamer_Details' table. You can move, or remove it, as needed.
+            this.gamer_DetailsTableAdapter.Fill(this.dataDataSet1.Gamer_Details);
+            try
+            {
+                connection.Open();
+                command.CommandText = "SELECT DISTINCT GENRE FROM Gamer_Details";
+                
+                OleDbDataReader read = command.ExecuteReader();
+                while(read.Read())
+                {
+                  cmbGenre.Items.Add(read.GetString(0));
+                }
+                read.Close();
+
+                command.CommandText = "SELECT DISTINCT GENDER FROM Gamer_Details";
+                
+                read = command.ExecuteReader();
+                while(read.Read())
+                {
+                  cmbGender.Items.Add(read.GetString(0));
+                }
+                read.Close();
+
+
+                
+            }
+            catch(OleDbException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                connection.Open();
+                string[] info = textBox1.Text.Split(' ');
+                string name = info[0];
+                string surname = info[1];
+                string prGame = textBox2.Text;
+                string genre = cmbGenre.SelectedItem.ToString();
+                string gender = cmbGender.SelectedItem.ToString();
+
+                command.CommandText = "INSERT INTO Gamer_Details(First_Name, Last_Name, PrefGame, Genre, Gender)" + 
+                                      " Values('" + name +  "', '" + surname +  "', '" + prGame +  "', '" + genre +  "', '" + gender +  "')";
+
+
+                command.ExecuteNonQuery();
+
+                command.CommandText = "SELECT COUNT(First_Name) FROM Gamer_Details";
+                MessageBox.Show("Your Gamer ID is No." + command.ExecuteScalar().ToString());
+
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally 
+            {
+                connection.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+
+                
+                command.CommandText = "UPDATE Gamer_Details SET PrefGame = '" + textBox3.Text + "' WHERE First_Name = '" + comboBox3.Text + "'"; ;
+                command.ExecuteNonQuery();
+
+                
+                MessageBox.Show("uPDATE successful");
+
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void fillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.gamer_DetailsTableAdapter2.FillBy(this.dataDataSet3.Gamer_Details);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
